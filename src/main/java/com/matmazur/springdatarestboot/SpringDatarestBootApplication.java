cbe43a11cc5eb884988d1bce15f117c71c51f314
Bingo Bobo <matmazur90@gmail.com>
2019-01-04 22:26:45 +0100

    initial commit

---
+++ src/main/java/com/matmazur/springdatarestboot/SpringDatarestBootApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.springdatarestboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatarestBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatarestBootApplication.class, args);
+    }
+
+}
+
