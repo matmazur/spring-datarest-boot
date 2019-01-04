ee64a3004e64f82ffc87638e35f6fce51a129005
Bingo Bobo <matmazur90@gmail.com>
2019-01-04 22:31:21 +0100

    added City model class

--- src/main/java/com/matmazur/springdatarestboot/SpringDatarestBootApplication.java
+++ src/main/java/com/matmazur/springdatarestboot/SpringDatarestBootApplication.java
@@ -1,5 +1,6 @@
 package com.matmazur.springdatarestboot;
 
+import com.matmazur.springdatarestboot.model.City;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDatarestBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDatarestBootApplication.class, args);
-    }
+        SpringApplication.run(SpringDatarestBootApplication.class, args);}
 
 }
 
