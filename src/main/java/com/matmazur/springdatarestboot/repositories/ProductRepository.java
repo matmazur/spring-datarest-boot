7be556ea82b0c222f964732ebe51c0dfe79c604a
Bingo Bobo <matmazur90@gmail.com>
2019-01-05 12:59:11 +0100

    commented changed main path to resource Product and changed visible name

--- src/main/java/com/matmazur/springdatarestboot/repositories/ProductRepository.java
+++ src/main/java/com/matmazur/springdatarestboot/repositories/ProductRepository.java
@@ -4,6 +4,6 @@ import com.matmazur.springdatarestboot.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "products")
+@RepositoryRestResource/*(path = "productarios", collectionResourceRel = "productarios")*/
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
