df11719cf8015721bdfedebc56390105baac904a
Bingo Bobo <matmazur90@gmail.com>
2019-01-05 12:21:56 +0100

    quick fix in path of ProductRepository, added wrappers for both POJO classes

--- src/main/java/com/matmazur/springdatarestboot/repositories/ProductRepository.java
+++ src/main/java/com/matmazur/springdatarestboot/repositories/ProductRepository.java
@@ -4,6 +4,6 @@ import com.matmazur.springdatarestboot.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "people")
+@RepositoryRestResource(path = "products")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
